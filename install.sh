86c29cbb5dd16ec56caa928ec7b4eaad87f10a28
rshariffdeen <rshariffdeen@gmail.com>
2016-05-31 12:17:53 +0530

    adding docker installation commands

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ printf "\nInstalling OrangeHRM System\n-----------------------------------------
 printf "Checking for Puppet Installation....\n";
 command -v puppet >/dev/null 2>&1 || {
     printf >&2 "Puppet is not installed.\nInstalling Puppet.....\n";
-    /bin/bash ./install_puppet_agent.sh;
+    sudo /bin/bash ./install_puppet_agent.sh;
     printf "Adding Puppet to your Path\n";
     export PATH=/opt/puppetlabs/bin:$PATH;
     printf "Starting Puppet Service\n";
@@ -11,4 +11,9 @@ command -v puppet >/dev/null 2>&1 || {
     printf "Finish installing puppet\n\n";
 }
 
-printf "Bootstrapping Puppet installation....\n";
+printf "Installing Docker module for Puppet\n";
+puppet module install garethr-docker
+
+printf "Installing Docker via Puppet....\n";
+puppet apply PuppetScripts/installDocker.pp
+
