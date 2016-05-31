24e32e448910635b09909144d4c951b683d2e212
rshariffdeen <rshariffdeen@gmail.com>
2016-05-31 17:50:27 +0530

    adding sudo

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ printf "\nInstalling OrangeHRM System\n-----------------------------------------
 printf "Checking for Puppet Installation....\n";
 command -v puppet >/dev/null 2>&1 || {
     printf >&2 "Puppet is not installed.\nInstalling Puppet.....\n";
-    /bin/bash ./install_puppet_agent.sh;
+    sudo /bin/bash ./install_puppet_agent.sh;
     printf "Adding Puppet to your Path\n";
     export PATH=/opt/puppetlabs/bin:$PATH;
     printf "Starting Puppet Service\n";
