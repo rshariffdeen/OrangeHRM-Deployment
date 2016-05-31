1612a558b5d1cce42090460ef2d7a380174031c3
rshariffdeen <rshariffdeen@gmail.com>
2016-05-31 16:21:26 +0530

    adding git ignore

--- PuppetScripts/deployOrangeHRM.pp
+++ PuppetScripts/deployOrangeHRM.pp
@@ -1,5 +1,5 @@
-include 'docker'
+include 'docker_compose'
 
-docker::image { '285645945015.dkr.ecr.us-east-1.amazonaws.com/ohrm-enterprise':
-   image_tag => '5.3v2',
+docker_compose { '../docker-compose.yml':
+  ensure  => present
 }
