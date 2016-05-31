8cc3fd8628a003433570d8c4aba86eede0e32962
rshariffdeen <rshariffdeen@gmail.com>
2016-05-31 20:08:40 +0530

    updating scripts

--- install.sh
+++ install.sh
@@ -12,18 +12,23 @@ command -v puppet >/dev/null 2>&1 || {
     printf "Finish installing puppet\n\n";
 }
 
-printf "Installing Docker module for Puppet\n";
+printf "\n\nInstalling Docker module for Puppet\n***************************************\n\n";
 puppet module install garethr-docker
 
-printf "Installing Docker Compose module for Puppet\n";
+printf "\n\nInstalling Docker Compose module for Puppet\n***********************************\n\n";
 puppet module install garystafford-docker_compose
 
-printf "Installing Docker via Puppet....\n";
+printf "\n\nInstalling Docker via Puppet....\n***********************************************\n\n";
 puppet apply PuppetScripts/installDocker.pp
 
-printf "Pulling OrangeHRM docker image...\n";
+printf "\n\nAdding current user to docker group\n*****************************************\n\n";
+sudo groupadd docker
+sudo usermod -aG docker $(whoami)
+sudo newgrp docker
+
+printf "\n\nPulling OrangeHRM docker image...\n*******************************************\n\n";
 docker login -u $aws_username -p $aws_token -e none https://285645945015.dkr.ecr.us-east-1.amazonaws.com
 docker pull $orangehrm_docker_image
 
-printf "Deploying OrangeHRM System....\n";
+printf "\n\nDeploying OrangeHRM System....\n*******************************************\n\n";
 puppet apply PuppetScripts/deployOrangeHRM.pp
