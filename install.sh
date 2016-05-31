dda12be4df6ca8faa339e9d4bba778f3f575ba5f
rshariffdeen <rshariffdeen@gmail.com>
2016-05-31 18:50:00 +0530

    updating install script

--- install.sh
+++ install.sh
@@ -12,14 +12,15 @@ command -v puppet >/dev/null 2>&1 || {
     printf "Finish installing puppet\n\n";
 }
 
+printf "Installing Docker\n";
+curl -fsSL https://get.docker.com/ | sh
+
 printf "Installing Docker module for Puppet\n";
 puppet module install garethr-docker
 
 printf "Installing Docker Compose module for Puppet\n";
 puppet module install garystafford-docker_compose
 
-printf "Installing Docker via Puppet....\n";
-puppet apply PuppetScripts/installDocker.pp
 
 printf "Deploying OrangeHRM System....\n";
 docker login -u AWS -p $aws_token -e none https://285645945015.dkr.ecr.us-east-1.amazonaws.com
