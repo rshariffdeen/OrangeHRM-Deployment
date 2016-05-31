703180fd9ae162099c3a23a0cb08ec7beec86f02
rshariffdeen <rshariffdeen@gmail.com>
2016-05-31 11:25:56 +0530

    adding initial scripts

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+printf "\nInstalling OrangeHRM System\n-----------------------------------------\n\n";
+printf "Checking for Puppet Installation....\n";
+command -v puppet >/dev/null 2>&1 || {
+    printf >&2 "Puppet is not installed.\nInstalling Puppet.....\n";
+    /bin/bash ./install_puppet_agent.sh;
+    printf "Adding Puppet to your Path\n";
+    export PATH=/opt/puppetlabs/bin:$PATH;
+    printf "Starting Puppet Service\n";
+    sudo service puppet start;
+    printf "Finish installing puppet\n\n";
+}
+
+printf "Bootstrapping Puppet installation....\n";
