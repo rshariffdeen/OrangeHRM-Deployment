b30b5974b9747df0c68db65a74f5de932300b2dd
rshariffdeen <rshariffdeen@gmail.com>
2016-06-01 12:10:24 +0530

    updating windows script

--- install_windows.ps1
+++ install_windows.ps1
@@ -25,24 +25,6 @@ function Get-InstalledApps
 $result = Get-InstalledApps | where {$_.DisplayName -like $appToMatch}
 
 If ($result -eq $null) {
-      Write-Host >&2 "Puppet is not installed.\nInstalling Puppet.....\n";
-      sudo /bin/bash ./install_puppet_agent.sh;
-      Write-Host "Adding Puppet to your Path\n";
-      export PATH=/opt/puppetlabs/bin:$PATH;
-      Write-Host "Starting Puppet Service\n";
-      sudo service puppet start;
-      Write-Host "Finish installing puppet\n\n";
+      Write-Host "Puppet is not installed.\nInstalling Puppet.....\n";
+
 }
-
-Write-Host "\n\nInstalling Docker module for Puppet\n***************************************\n\n";
-sudo /opt/puppetlabs/bin/puppet module install garethr-docker
-
-Write-Host "\n\nInstalling Docker Compose module for Puppet\n***********************************\n\n";
-sudo /opt/puppetlabs/bin/puppet module install garystafford-docker_compose
-
-Write-Host "\n\nInstalling Docker via Puppet\n***********************************************\n\n";
-sudo /opt/puppetlabs/bin/puppet apply PuppetScripts/installDocker.pp
-
-Write-Host "\n\nAdding current user to docker group\n*****************************************\n\n";
-sudo usermod -aG docker $(whoami)
-newgrp docker
