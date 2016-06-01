98ef84bf4891a1960096d98129efbb8ea428651d
rshariffdeen <rshariffdeen@gmail.com>
2016-06-01 16:15:44 +0530

    adding exit command to install script

--- install.sh
+++ install.sh
@@ -23,3 +23,4 @@ sudo /opt/puppetlabs/bin/puppet apply PuppetScripts/installDocker.pp
 printf "\n\nAdding current user to docker group\n*****************************************\n\n";
 sudo usermod -aG docker $(whoami)
 newgrp docker
+exit 0
